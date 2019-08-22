38813089553adc05a9b75e75ca8306b4c44cf885
Jack Steam <teamsteamdev@gmail.com>
2019-08-22 10:52:57 -0500

    Add prototype code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
-import delay from 'delay'
+import {
+  createContextMenu,
+  createContextMenuStream,
+} from './create'
+import {
+  updateContextMenu,
+  updateContextMenuStream,
+} from './update'
+import {
+  removeContextMenu,
+  removeContextMenuStream,
+} from './remove'
+import { contextMenuClickStream } from './clickStream'
 
-/**
- * Export anyFunction
- */
-export const anyFunction = delay(500).then(() => 'delayed')
+export const menus = {
+  create: createContextMenu,
+  update: updateContextMenu,
+  remove: removeContextMenu,
+  // removeAll: removeAllContextMenus,
+  createStream: createContextMenuStream.asObservable(),
+  updateStream: updateContextMenuStream.asObservable(),
+  removeStream: removeContextMenuStream.asObservable(),
+  // removeAllStream: removeAllContextMenusStream.asObservable(),
+  clickStream: contextMenuClickStream,
+}
