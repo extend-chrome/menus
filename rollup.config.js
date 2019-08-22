d49b83fda591e8dae02d2798b58de4d3d6b61081
Jack Steam <jacksteamdev@gmail.com>
2019-08-22 15:50:59 +0000

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+/* eslint-env node */
+
+import typescript from 'rollup-plugin-typescript'
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'lib/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+      {
+        file: 'lib/index.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript()],
+  },
+]
