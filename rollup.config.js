f17853247b98b1a7744782e31a3ac7b96d795aa3
Jack Steam <teamsteamdev@gmail.com>
2019-10-23 15:30:22 -0500

    Fix rollup output config

--- rollup.config.js
+++ rollup.config.js
@@ -8,12 +8,12 @@ export default [
     input: 'src/index.ts',
     output: [
       {
-        file: 'lib/index.esm.js',
+        file: 'lib/index-esm.js',
         format: 'esm',
         sourcemap: true,
       },
       {
-        file: 'lib/index.cjs.js',
+        file: 'lib/index-cjs.js',
         format: 'cjs',
         sourcemap: true,
       },
