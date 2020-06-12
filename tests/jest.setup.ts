814a14f0b1909cc2fcafe5284dd0f422b3a29032
Jack Steam <jacksteamdev@gmail.com>
2020-06-12 11:29:43 -0500

    Smash tests

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,14 +1,4 @@
-import * as chrome from 'sinon-chrome'
-;(<any>window).chrome = chrome
-
-var ChromePromise = require('chrome-promise/constructor')
-var chromep = new ChromePromise({ chrome })
-;(<any>window).chromep = chromep
-
-// Jest's jsdom does not include window.crypto
-var nodeCrypto = require('crypto')
-;(<any>window).crypto = {
-  getRandomValues: function(buffer: Uint8Array) {
-    return nodeCrypto.randomFillSync(buffer)
-  },
-}
+// var ChromePromise = require('chrome-promise/constructor')
+// var chromep = new ChromePromise({ chrome })
+// // eslint-disable-next-line no-undef
+// Object.assign(global, { chromep })
--- tests/units/sample.test.ts
+++ tests/units/sample.test.ts
@@ -1,15 +1,15 @@
-import * as chrome from 'sinon-chrome'
-// import assert from 'power-assert'
+// import * as chrome from 'sinon-chrome'
+// // import assert from 'power-assert'
 
-let lastError: { message: string } | undefined
-const lastErrorSpy = jest.fn(() => lastError)
-Object.defineProperty(chrome.runtime, 'lastError', {
-  get: lastErrorSpy,
-})
+// let lastError: { message: string } | undefined
+// const lastErrorSpy = jest.fn(() => lastError)
+// Object.defineProperty(chrome.runtime, 'lastError', {
+//   get: lastErrorSpy,
+// })
 
-afterEach(() => {
-  lastError = undefined
-  chrome.reset()
-})
+// afterEach(() => {
+//   lastError = undefined
+//   chrome.reset()
+// })
 
 test.todo('write some tests!')
